4e3957f452a9e15f06265255ef7d3e0de270f9eb
wicultydotcom <support@wiculty.com>
2021-03-05 22:44:40 +0530

    real script

--- 5.Jenkins/multi-deploy.sh
+++ 5.Jenkins/multi-deploy.sh
@@ -5,4 +5,7 @@ do
 echo "deploying the code to $i ..."
 sleep 3
 echo "deployment to $i is succussful!"
+sshpass -p gamut scp target/gamutgurus.war gamut@$i:/home/gamut/Distros/apache-tomcat-8.5.63/webapps
+sshpass -p $PASSWORD ssh gamut@$i "/home/gamut/Distros/apache-tomcat-8.5.63/bin/startup.sh"
+
 done
